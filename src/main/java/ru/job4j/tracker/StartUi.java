ad1fb930efc46e871ada93e750eeea8e44c29e06
Daniil <kattsyn.dd@gmail.com>
2024-04-20 13:12:39 +0300

    10. toString [#310018 #170243]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -1,12 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUi {
 
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(timeFormatter.format(item.getCreated()));
+        System.out.println(item);
     }
 }
